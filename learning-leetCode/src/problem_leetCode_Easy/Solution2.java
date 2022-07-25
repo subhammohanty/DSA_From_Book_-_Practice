65ffa3a9327244d043591af0e2ed54a759a2814a
Subham Mohanty <submohan4@publicisgroupe.net>
2022-07-25 16:43:09 +0530

    ss

--- learning-leetCode/src/problem_leetCode_Easy/Solution2.java
+++ learning-leetCode/src/problem_leetCode_Easy/Solution2.java
@@ -16,4 +16,14 @@ public class Solution2 {
 		return result;
 
 	}
+	
+	public static boolean isPalindromOpimise(int x) {
+		if(x < 0 || (x != 0 && x%10 == 0)) {return false;}
+		int res = 0;
+		while(x > res) {
+			res = res * 10 + x %10;
+			x = x / 10;
+		}
+		return (x == res || x == res/10);
+	}
 }
